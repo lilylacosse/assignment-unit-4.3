4f5dc32fefea620c7050568c69589d2c925c1b84
Lily LaCosse <lilylacosse@gmail.com>
2023-02-08 19:04:05 -0600

    Completed stretch goal #3

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,12 +1,19 @@
 console.log('***** Cart Functions *****');
 
 let basket = [];
+const maxItems = 5;
 
 console.log('Basket is:', basket);
 
+//addItem adjusted for stretch goal #3
 function addItem(item) {
-    basket.push(item);
-    return true;
+    if (isFull(basket) === false) {
+        basket.push(item);
+        return true;
+    }
+    else {
+        return false;
+    }
 }   
 
 console.log('Adding apples (expect true):', addItem('apples'));
@@ -21,6 +28,8 @@ function listItems(array) {
 console.log('Adding bananas:', addItem('bananas'));
 console.log('Adding kiwis:', addItem('kiwis'));
 console.log('Adding pineapples:', addItem('pineapples'));
+console.log('Adding grapefruit:', addItem('grapefruit'));
+console.log('Adding peaches (expect false) :', addItem('peaches'));
 console.log('Basket is now:', basket);
 
 console.log('About to call listItems w. input of basket - expect a log of types of fruit:')
@@ -39,3 +48,34 @@ console.log('basket after calling empty:', basket)
 
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// STRETCH GOALS
+// Stretch goal #1 on line 4
+// #2
+
+function isFull(array) {
+    if (array.length < maxItems) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+console.log('basket is:', basket);
+console.log('Test isFull - expect false:', isFull(basket));
+
+addItem('apples');
+addItem('grapes');
+addItem('popsicles');
+addItem('sourdough bread');
+addItem('hummus');
+
+console.log('basket is:', basket);
+console.log('Test isFull - expect true:', isFull(basket));
+
+addItem('chips');
+
+console.log('Test isFull - expect true:', isFull(basket));
+
+// Stretch goal #3 added at line 9 
+
